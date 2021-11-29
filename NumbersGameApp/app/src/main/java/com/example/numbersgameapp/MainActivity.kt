2dacd3242ca4b7cd4515666edeca57b83de566d1
arwaalmashi <ar.al-mashi@hotmail.com>
2021-11-29 06:15:13 +0300

    Number game finished

--- NumbersGameApp/app/src/main/java/com/example/numbersgameapp/MainActivity.kt
+++ NumbersGameApp/app/src/main/java/com/example/numbersgameapp/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.numbersgameapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
 
 lateinit var recyclerView : RecyclerView
 lateinit var editText: EditText
 lateinit var sendButton: Button
+var count : Int = 3
 
 var textList : ArrayList<MyText> = ArrayList()
 val textRecycleView : TextRecycleView by lazy { TextRecycleView() }
@@ -23,15 +26,36 @@ class MainActivity : AppCompatActivity() {
         sendButton = findViewById(R.id.sendButton)
 
         recyclerView.adapter = textRecycleView
+        var randomNumber: Int = Random.nextInt(11)
+
+        textList.add(MyText("Random =  ${randomNumber}"))
+        textRecycleView.setText(textList)
 
         sendButton.setOnClickListener {
-            textList.add(MyText("You guess ${editText.text.toString()}"))
+
+            var userNumber: Editable? = editText.text
+            textList.add(MyText("You guess ${userNumber}"))
             textRecycleView.setText(textList)
             editText.setText("")
 
-            textList.add(MyText("You guess ${editText.text.toString()}"))
-            textRecycleView.setText(textList)
-            editText.setText("")
+            if ("$randomNumber" == userNumber.toString()) {
+                textList.add(MyText("Success"))
+                textRecycleView.setText(textList)
+                textList.add(MyText("Finish Game"))
+                textRecycleView.setText(textList)
+            } else {
+                if (count == 1) {
+                    textList.add(MyText("Finish Game, Correct number = $randomNumber"))
+                    textRecycleView.setText(textList)
+                } else {
+                    count--
+                    textList.add(MyText("You have ${count} guesses left"))
+                    textRecycleView.setText(textList)
+                }
+
+            }
+
+
         }
     }
 }
